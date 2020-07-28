a0b8487db3a853896eb6bbf277c392fdce2188dd
GSRdevelopment <gsrdevelopment.contact@gmail.com>
2020-07-28 02:05:14 -0400

    first commit

---
+++ precache-manifest.1b0538766213025f869479f82272323d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5971ed881a42d555cb96f49cc3af7e72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c917eb76396f86991cda",
+    "url": "/static/css/main.03c6ca28.chunk.css"
+  },
+  {
+    "revision": "91b964a2e1da00fe6960",
+    "url": "/static/js/2.5505b98e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5505b98e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c917eb76396f86991cda",
+    "url": "/static/js/main.0a08aac5.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
